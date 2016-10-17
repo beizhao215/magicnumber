7a5afa0e4cec520ccddf3e007b8d6dba08fbc922
BEI ZHAO <beizhao@BEIdeMacBook-Pro.local>
2016-10-17 15:29:16 -0700

    hide token

--- webapp/app/static/js/myapp.js
+++ webapp/app/static/js/myapp.js
@@ -1,5 +1,5 @@
 var mymap = L.map('mapid').setView([40.712800, -74.005900], 11);
-L.tileLayer('https://api.mapbox.com/styles/v1/beizhao215/ciu2cjsga006e2iqd93u9vfe4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmVpemhhbzIxNSIsImEiOiJjaXExcXhkM3UwMHlhZnNubm5uaG8wenJnIn0.cAFVYL166sWTuqwT_zwSzg', {
+L.tileLayer('https://api.mapbox.com/styles/v1/beizhao215/ciu2cjsga006e2iqd93u9vfe4/tiles/256/{z}/{x}/{y}?access_token=my_token', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'insight'
